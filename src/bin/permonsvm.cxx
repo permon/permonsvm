e804d1934d0a10183abdbeaae305165183eaa07e
Vaclav Hapla <vaclav.hapla@vsb.cz>
2017-03-22 12:48:17 +0100

    permonsvm.cxx: initial support for parallel runs

--- src/bin/permonsvm.cxx
+++ src/bin/permonsvm.cxx
@@ -11,30 +11,93 @@ static MPI_Comm     comm;
 static PetscMPIInt  rank, commsize;
 
 #undef __FUNCT__
+#define __FUNCT__ "testSVM_load_data_from_file"
+PetscErrorCode testSVM_load_data_from_file(const char *filename, PetscInt n_examples, Mat *Xt_new, Vec *y_new)
+{
+  excape::DataParser parser;
+  Mat Xt_seq=NULL, Xt=NULL;
+  Vec y_seq=NULL, y=NULL;
+  PetscViewer viewer=NULL;
+  VecScatter scatter=NULL;
+    
+  PetscFunctionBeginI;
+  parser.SetInputFileName(filename);
+  //TRY( parser.GetData(comm, n_examples, &Xt, &y) );
+  
+  //TODO workaround until parser works in parallel
+  //TODO try to use MatDistribute_MPIAIJ
+  if (!rank) {
+    TRY( parser.GetData(PETSC_COMM_SELF, n_examples, &Xt_seq, &y_seq) );
+    TRY( PetscViewerBinaryOpen(PETSC_COMM_SELF, "mat_Xt_tmp.bin", FILE_MODE_WRITE, &viewer) );
+    TRY( MatView(Xt_seq, viewer) );
+    TRY( PetscViewerDestroy(&viewer) );
+    TRY( MatDestroy(&Xt_seq) );
+  } else {
+    TRY( VecCreateSeq(PETSC_COMM_SELF, 0, &y_seq) );
+  }
+  
+  TRY( PetscViewerBinaryOpen(comm, "mat_Xt_tmp.bin", FILE_MODE_READ, &viewer) );
+  TRY( MatCreate(comm, &Xt) );
+  TRY( MatSetType(Xt, MATAIJ) );
+  TRY( MatLoad(Xt, viewer) );
+  TRY( PetscViewerDestroy(&viewer) );
+  
+  TRY( MatCreateVecs(Xt, NULL, &y) );
+  TRY( VecScatterCreateToZero(y,&scatter,NULL) );
+  TRY( VecScatterBegin(scatter,y_seq,y,INSERT_VALUES,SCATTER_REVERSE) );
+  TRY( VecScatterEnd(  scatter,y_seq,y,INSERT_VALUES,SCATTER_REVERSE) );
+  TRY( VecScatterDestroy(&scatter) );
+  TRY( VecDestroy(&y_seq) );
+
+  *Xt_new = Xt;
+  *y_new = y;
+  PetscFunctionReturnI(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "testSVM_load_data"
+PetscErrorCode testSVM_load_data(Mat *Xt, Vec *y, Mat *Xt_test, Vec *y_test)
+{
+  PetscInt       M,N;
+  char           filename[PETSC_MAX_PATH_LEN] = "dummy.txt";
+  char           filename_test[PETSC_MAX_PATH_LEN] = "";
+  PetscInt       n_examples = PETSC_DEFAULT;  /* PETSC_DEFAULT or PETSC_DECIDE means all */
+  PetscBool      filename_test_set = PETSC_FALSE;
+
+  PetscFunctionBeginI;
+  TRY( PetscOptionsGetString(NULL,NULL,"-f",filename,sizeof(filename),NULL) );
+  TRY( PetscOptionsGetString(NULL,NULL,"-f_test",filename_test,sizeof(filename_test),&filename_test_set) );
+  TRY( PetscOptionsGetInt(NULL,NULL,"-n_examples",&n_examples,NULL));
+
+  TRY( testSVM_load_data_from_file(filename, n_examples, Xt, y) );
+  TRY( MatGetSize(*Xt, &M, &N));
+  TRY( PetscPrintf(comm, "\n\n### PermonSVM: loaded %d training examples with %d attributes from file %s\n",M,N,filename));
+
+  if (filename_test_set) {
+    TRY( testSVM_load_data_from_file(filename_test, PETSC_DECIDE, Xt_test, y_test) );
+    TRY( MatGetSize(*Xt_test, &M, &N));
+    TRY( PetscPrintf(comm, "### PermonSVM: loaded %d testing examples with %d attributes from file %s\n",M,N,filename_test));
+  } else {
+    *Xt_test = NULL;
+    *y_test = NULL;
+  }
+
+  TRY( PetscPrintf(comm, "\n") );
+  PetscFunctionReturnI(0);
+}
+
+#undef __FUNCT__
 #define __FUNCT__ "testSVM_files"
 PetscErrorCode testSVM_files()
 {
-    excape::DataParser parser;
     PermonSVM svm;
     PetscReal C;
-    PetscInt N_all, N_eq, M, N;
+    PetscInt N_all, N_eq;
     Mat Xt, Xt_test;
     Vec y, y_test;
-    char           filename[PETSC_MAX_PATH_LEN] = "dummy.txt";
-    char           filename_test[PETSC_MAX_PATH_LEN] = "";
-    PetscInt       n_examples = PETSC_DEFAULT;  /* PETSC_DEFAULT or PETSC_DECIDE means all */
-    PetscBool      filename_test_set = PETSC_FALSE;
     
     PetscFunctionBeginI;
-    TRY( PetscOptionsGetString(NULL,NULL,"-f",filename,sizeof(filename),NULL) );
-    TRY( PetscOptionsGetString(NULL,NULL,"-f_test",filename_test,sizeof(filename_test),&filename_test_set) );
-    TRY( PetscOptionsGetInt(NULL,NULL,"-n_examples",&n_examples,NULL));
-    
-    parser.SetInputFileName(filename);
-    TRY( parser.GetData(comm, n_examples, &Xt, &y) );
-    
-    TRY( MatGetSize(Xt, &M, &N));
-    TRY( PetscPrintf(comm, "\n\n### PermonSVM: loaded %d examples with %d attributes from file %s\n\n",M,N,filename));
+    TRY( testSVM_load_data(&Xt, &y, &Xt_test, &y_test) );
     
     /* ------------------------------------------------------------------------ */
     TRY( PermonSVMCreate(comm, &svm) );
@@ -46,9 +109,7 @@ PetscErrorCode testSVM_files()
     TRY( PetscPrintf(comm, "\n### PermonSVM: %8d of %8d training examples classified correctly (%.2f%) with C = %1.1e\n", N_eq, N_all, ((PetscReal)N_eq)/((PetscReal)N_all)*100.0, C) );
 
     /* ------------------------------------------------------------------------ */ 
-    if (filename_test_set) {
-      parser.SetInputFileName(filename_test);
-      TRY( parser.GetData(comm, PETSC_DEFAULT, &Xt_test, &y_test) );
+    if (Xt_test) {
       TRY( PermonSVMTest(svm, Xt_test, y_test, &N_all, &N_eq) );
       TRY( PermonSVMGetPenalty(svm, &C) );
       TRY( PetscPrintf(comm, "### PermonSVM: %8d of %8d  testing examples classified correctly (%.2f%) with C = %1.1e\n", N_eq, N_all, ((PetscReal)N_eq)/((PetscReal)N_all)*100.0, C) );
@@ -73,8 +134,6 @@ int main(int argc, char** argv)
   comm = PETSC_COMM_WORLD;
   TRY( MPI_Comm_rank(comm, &rank) );
   TRY( MPI_Comm_size(comm, &commsize) );
-
-  FLLOP_ASSERT(commsize==1,"currently only sequential");
 
   TRY( PetscPrintf(comm, "PETSC_DIR:\t" PETSC_DIR "\n") );
   TRY( PetscPrintf(comm, "PETSC_ARCH:\t" PETSC_ARCH "\n") );
