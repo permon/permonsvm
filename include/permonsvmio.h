7548457878377bb9aef901be4717358a04069077
Marek Pecha <marek.pecha@vsb.cz>
2018-10-21 09:32:36 +0200

    Add SVMLight loader skeleton

---
+++ include/permonsvmio.h
@@ -0,0 +1,8 @@
+#if !defined(__PERMONSVMIO_H)
+#define	__PERMONSVMIO_H
+
+#include <permonsvm.h>
+
+FLLOP_EXTERN PetscErrorCode PermonSVMLoadData(const char *filename,Mat *,Vec *);
+
+#endif //__PERMONSVMIO_H
---
+++ src/svm/interface/io.c
@@ -0,0 +1,15 @@
+#include <permonsvm.h>
+
+/*
+ *
+ */
+#undef __FUNCT__
+#define __FUNCT__ "PermonSVMLoadData"
+PetscErrorCode PermonSVMLoadData(const char filename[],Mat *Xt,Vec *y) {
+
+  PetscFunctionBeginI;
+  *Xt = NULL;
+  *y = NULL;
+  PetscFunctionReturnI(0);
+}
+
