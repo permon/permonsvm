6cb8a7d0977585e177e448bc17c8b00d8b5b4946
Marek Pecha <marek.pecha@vsb.cz>
2018-10-22 09:26:41 +0200

    PermonSVMRunBinaryClassification impl

--- src/bin/permonsvmfile.c
+++ src/bin/permonsvmfile.c
@@ -1,5 +1,6 @@
 #include <mpi.h>
 #include <permonsvm.h>
+#include <permonsvmio.h>
 
 static MPI_Comm    comm;
 static PetscMPIInt comm_rank,comm_size;
@@ -7,7 +8,32 @@ static PetscMPIInt comm_rank,comm_size;
 #undef __FUNCT__
 #define __FUNCT__ "PermonSVMRunBinaryClassification"
 PetscErrorCode PermonSVMRunBinaryClassification() {
-  
+  PermonSVM svm;
+  PetscReal C;
+  PetscInt  N_all, N_eq;
+  Mat       Xt_training;
+  Vec       y_training;
+  char      training_file[PETSC_MAX_PATH_LEN] = "dummy.txt";
+
+  PetscFunctionBeginI;
+  TRY( PermonSVMCreate(comm,&svm) );
+  TRY( PermonSVMSetFromOptions(svm) );
+
+  TRY( PermonSVMLoadData(training_file,&Xt_training,&y_training) );
+
+  if (Xt_training && y_training) {
+    TRY( PermonSVMSetTrainingSamples(svm,Xt_training,y_training) );
+    TRY( PermonSVMTrain(svm) );
+    TRY( PermonSVMTest(svm,Xt_training,y_training,&N_all,&N_eq) );
+    TRY( PermonSVMGetC(svm,&C) );
+    TRY( PetscPrintf(comm,"\n### PermonSVM: %8d of %8d training samples classified correctly (%.2f%%) with C = %1.1e\n",
+                     N_eq,N_all,((PetscReal)N_eq)/((PetscReal)N_all)*100.0,C) );
+  }
+
+  if (Xt_training) TRY( MatDestroy(&Xt_training) );
+  if (y_training) TRY( VecDestroy(&y_training) );
+  TRY( PermonSVMDestroy(&svm) );
+  PetscFunctionReturnI(0);
 }
 
 #undef __FUNCT__
