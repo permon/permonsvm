2b620bb343bca6a685c587055bffce7eca203294
“Marek <marek.pecha@vsb.cz>
2019-01-20 21:10:46 +0100

    SVMCrossValidation_Binary: remove SVMSetFromOptions

--- src/svm/impls/binary/binary.c
+++ src/svm/impls/binary/binary.c
@@ -1189,22 +1189,20 @@ PetscErrorCode SVMGridSearch_Binary(SVM svm)
 #define __FUNCT__ "SVMCrossValidation_Binary"
 PetscErrorCode SVMCrossValidation_Binary(SVM svm,PetscReal c_arr[],PetscInt m,PetscReal score[])
 {
-  MPI_Comm comm;
-  SVM cross_svm;
+  MPI_Comm    comm;
+  SVM         cross_svm;
 
-  QP  qp;
-  QPS qps;
+  QP          qp;
+  QPS         qps;
 
-  Mat      Xt,Xt_training,Xt_test;
-  Vec      x,y,y_training,y_test;
+  Mat         Xt,Xt_training,Xt_test;
+  Vec         x,y,y_training,y_test;
 
-  PetscInt  lo,hi,first,n;
-  PetscInt  i,j,nfolds;
-  PetscReal s;
+  PetscInt    lo,hi,first,n;
+  PetscInt    i,j,nfolds;
+  PetscReal   s;
 
-  IS       is_training,is_test;
-
-  const char *prefix;
+  IS          is_training,is_test;
 
   PetscInt    svm_mod;
   SVMLossType svm_loss;
@@ -1215,30 +1213,29 @@ PetscErrorCode SVMCrossValidation_Binary(SVM svm,PetscReal c_arr[],PetscInt m,Pe
 
   PetscFunctionBegin;
   TRY( PetscObjectGetComm((PetscObject) svm,&comm) );
-
-  TRY( SVMGetNfolds(svm,&nfolds) );
   TRY( SVMGetLossType(svm,&svm_loss) );
   TRY( SVMGetMod(svm,&svm_mod) );
-  TRY( SVMGetCrossValidationScoreType(svm,&model_score) );
+
+  /* Create SVM used in cross validation */
+  TRY( SVMCreate(PetscObjectComm((PetscObject)svm),&cross_svm) );
+  TRY( SVMSetType(cross_svm,SVM_BINARY) );
+
+  /* Set options of SVM used in cross validation */
+  TRY( SVMSetMod(cross_svm,svm_mod) );
+  TRY( SVMSetLossType(cross_svm,svm_loss) );
+  TRY( SVMAppendOptionsPrefix(cross_svm,"cross_") );
+  TRY( SVMSetFromOptions(cross_svm) );
+
+  TRY( PetscOptionsHasName(NULL,((PetscObject)cross_svm)->prefix,"-svm_info",&info_set) );
 
   TRY( SVMGetTrainingDataset(svm,&Xt,&y) );
   TRY( MatGetOwnershipRange(Xt,&lo,&hi) );
 
-  TRY( SVMCreate(PetscObjectComm((PetscObject)svm),&cross_svm) );
-  TRY( SVMSetType(cross_svm,SVM_BINARY) );
-
-  TRY( SVMSetMod(cross_svm,svm_mod) );
-  TRY( SVMSetLossType(cross_svm,svm_loss) );
-
-  TRY( SVMGetOptionsPrefix(svm,&prefix) );
-  TRY( SVMAppendOptionsPrefix(cross_svm,"cross_") );
-  TRY( SVMSetFromOptions(cross_svm) );
-
   TRY( ISCreate(PetscObjectComm((PetscObject) svm),&is_test) );
   TRY( ISSetType(is_test,ISSTRIDE) );
 
-  TRY( PetscOptionsHasName(NULL,((PetscObject)cross_svm)->prefix,"-svm_info",&info_set) );
-
+  TRY( SVMGetCrossValidationScoreType(svm,&model_score) );
+  TRY( SVMGetNfolds(svm,&nfolds) );
   for (i = 0; i < nfolds; ++i) {
     if (info_set) {
       TRY( PetscPrintf(comm,"SVM: fold %d of %d\n",i+1,nfolds) );
@@ -1248,10 +1245,12 @@ PetscErrorCode SVMCrossValidation_Binary(SVM svm,PetscReal c_arr[],PetscInt m,Pe
     if (first < lo) first += nfolds;
     n = (hi + nfolds - first - 1) / nfolds;
 
+    /* Create test dataset */
     TRY( ISStrideSetStride(is_test,n,first,nfolds) );
     TRY( MatCreateSubMatrix(Xt,is_test,NULL,MAT_INITIAL_MATRIX,&Xt_test) );
     TRY( VecGetSubVector(y,is_test,&y_test) );
 
+    /* Create training dataset */
     TRY( ISComplement(is_test,lo,hi,&is_training) );
     TRY( MatCreateSubMatrix(Xt,is_training,NULL,MAT_INITIAL_MATRIX,&Xt_training) );
     TRY( VecGetSubVector(y,is_training,&y_training) );
@@ -1283,13 +1282,9 @@ PetscErrorCode SVMCrossValidation_Binary(SVM svm,PetscReal c_arr[],PetscInt m,Pe
       score[j] += s;
 
       TRY( SVMGetQPS(cross_svm,&qps) );
-      TRY( QPSResetStatistics(qps) );
     }
-
     TRY( SVMReset(cross_svm) );
-    TRY( SVMSetLossType(cross_svm,svm_loss) );
-    TRY( SVMSetMod(cross_svm,svm_mod) );
-    TRY( SVMSetFromOptions(cross_svm) );
+    TRY( QPSResetStatistics(qps) );
 
     TRY( VecRestoreSubVector(y,is_training,&y_training) );
     TRY( MatDestroy(&Xt_training) );
