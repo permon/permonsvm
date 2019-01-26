20af49c764cbf2253028a768103aa5ccdb5b6ca5
“Marek <marek.pecha@vsb.cz>
2019-01-26 10:03:05 +0100

    Move SVM_Binary binary.c -> binaryimpl.h

--- src/svm/impls/binary/binary.c
+++ src/svm/impls/binary/binary.c
@@ -1,51 +1,13 @@
 
-#include <permon/private/svmimpl.h>
+#include "binaryimpl.h"
 
 PetscClassId SVM_CLASSID;
 
 const char *const SVMLossTypes[]={"L1","L2","SVMLossType","SVM_",0};
 
 typedef struct {
-  Mat         Xt_training;
-  Vec         y_training;
-  Vec         y_inner;
-  IS          is_p,is_n;
-
-  PetscScalar y_map[2];
-  Vec         diag;
-  Mat         D;
-
-  Vec         w;
-  PetscScalar b;
-
-  PetscScalar hinge_loss;
-
-  PetscReal   norm_w;
-  PetscReal   margin;
-
-  IS          is_sv;
-  PetscInt    nsv;
-
-  QPS         qps;
-  PetscInt    svm_mod;
-
-  PetscInt    confusion_matrix[4];
-  PetscReal   model_scores[5];
-
-  /* Work vecs */
-  Vec         work[3]; /* xi, c, Xtw */
-
-  /* Valuess of primal and dual objective functions */
-  PetscReal   primalObj,dualObj;
-} SVM_Binary;
-
-typedef struct {
   SVM svm_inner;
 } SVM_Binary_mctx;
-
-FLLOP_EXTERN PetscErrorCode SVMCrossValidation_Binary(SVM,PetscReal [],PetscInt,PetscReal []);
-FLLOP_EXTERN PetscErrorCode SVMKFoldCrossValidation_Binary(SVM,PetscReal [],PetscInt,PetscReal []);
-FLLOP_EXTERN PetscErrorCode SVMStratifiedKFoldCrossValidation_Binary(SVM,PetscReal [],PetscInt,PetscReal []);
 
 static PetscErrorCode SVMMonitorCreateCtx_Binary(void **,SVM);
 static PetscErrorCode SVMMonitorDestroyCtx_Binary(void **);
---
+++ src/svm/impls/binary/binaryimpl.h
@@ -0,0 +1,44 @@
+
+#if !defined(__BINARYIMPL_H)
+#define	__BINARYIMPL_H
+
+#include <permon/private/svmimpl.h>
+
+typedef struct {
+    Mat         Xt_training;
+    Vec         y_training;
+    Vec         y_inner;
+    IS          is_p,is_n;
+
+    PetscScalar y_map[2];
+    Vec         diag;
+    Mat         D;
+
+    Vec         w;
+    PetscScalar b;
+
+    PetscScalar hinge_loss;
+
+    PetscReal   norm_w;
+    PetscReal   margin;
+
+    IS          is_sv;
+    PetscInt    nsv;
+
+    QPS         qps;
+    PetscInt    svm_mod;
+
+    PetscInt    confusion_matrix[4];
+    PetscReal   model_scores[5];
+
+    /* Work vecs */
+    Vec         work[3]; /* xi, c, Xtw */
+
+    /* Valuess of primal and dual objective functions */
+    PetscReal   primalObj,dualObj;
+} SVM_Binary;
+
+FLLOP_EXTERN PetscErrorCode SVMCrossValidation_Binary(SVM,PetscReal [],PetscInt,PetscReal []);
+FLLOP_EXTERN PetscErrorCode SVMKFoldCrossValidation_Binary(SVM,PetscReal [],PetscInt,PetscReal []);
+FLLOP_EXTERN PetscErrorCode SVMStratifiedKFoldCrossValidation_Binary(SVM,PetscReal [],PetscInt,PetscReal []);
+#endif
